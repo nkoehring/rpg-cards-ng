cadb759fbac255d1fc97cd8e4902d2696d5666cf
koehr <n@koehr.in>
2020-05-25 19:49:54 +0200

    removes obsolete editor component

--- src/editor/index.ts
+++ src/editor/index.ts
@@ -1,5 +1,4 @@
 export { default as Delimiter } from './delimiter'
 export { default as Heading } from './heading'
-export { default as List } from './list'
 export { default as Charges } from './charges'
 export { default as DnDStats } from './dnd-stats'
